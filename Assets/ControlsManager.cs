d7c0bb1111743392408fa23d4a90ba4c03827853
Nils Munch <gecko@wolflord.dev>
2021-10-26 11:51:27 +0200

    Basic movement

---
+++ Assets/ControlsManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LegoInterview
+{
+    public class ControlsManager : MonoBehaviour
+    {
+        public Player controlledPlayer;
+        void Update()
+        {
+            Vector3 movement = new Vector3(0, 0, 0);
+            if (Input.GetKey(KeyCode.W))
+            {
+                movement += new Vector3(0, 0, 1);
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                movement += new Vector3(0, 0, -1);
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                movement += new Vector3(-1, 0, 0);
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                movement += new Vector3(1, 0, 0);
+            }
+
+            controlledPlayer.MoveInDirection(movement);
+        }
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LegoInterview
+{
+
+    public class Player : MonoBehaviour
+    {
+        [Header("Movement")]
+        public float movementSpeed = 10f;
+        public void MoveInDirection(Vector3 movement)
+        {
+            if (movement == Vector3.zero) return;
+            transform.position += movement * Time.deltaTime * movementSpeed;
+
+            float singleStep = movementSpeed * Time.deltaTime;
+            Vector3 newDirection = Vector3.RotateTowards(transform.forward, movement, singleStep, 0.0f);
+            Debug.DrawRay(transform.position, newDirection, Color.red);
+            transform.rotation = Quaternion.LookRotation(newDirection);
+        }
+
+    }
+}
